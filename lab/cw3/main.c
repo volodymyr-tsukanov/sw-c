90e524387c67e16ca8195b1e4401a25fcec87f5d
Volodymyr <134626794+volodymyr-tsukanov@users.noreply.github.com>
2024-11-01 10:06:36 +0100

    cw3 (multi-num 8b calculator)

--- lab/cw3/main.c
+++ lab/cw3/main.c
@@ -2,9 +2,19 @@
  * CW3
  *
  * Created: 17.10.2024 08:27:59
- * Author : Volodymyr Tsukanov
+ * Author : volodymyr-tsukanov
  */
 #define F_CPU 4000000	//częstotliwość procesora dla delay
+
+#define CLC_OPT_ADD 0b00010000	//16
+#define CLC_OPT_SUB 0b00100000	//32
+#define CLC_OPT_MUL 0b01000000	//64
+#define CLC_OPT_DIV 0b10000000	//128
+#define CLC_FDB_DEF 0xFF  //default
+#define CLC_FDB_RES 0b01100110	//result
+#define CLC_FDB_RST 0b01111110	//reset
+#define CLC_FDB_EXC 0b10101010	//exception
+
 
 #include <avr/io.h>
 #include <util/delay.h>
@@ -13,7 +23,7 @@ typedef enum {
 	PORT_A,
 	PORT_B,
 	PORT_C,
-	PORT_D,
+	PORT_D
 } port_name_t;
 
 
@@ -65,52 +75,105 @@ char get_key(){	//dla portA
 
 	return res;
 }
-void calculus_feedback(port_name_t diod_port, uint8_t val){
-	set_diodes(diod_port, val);
-	_delay_ms(1700);
+
+void calculus_feedback(port_name_t diod_port, uint8_t value){
+	set_diodes(diod_port, value);
+	_delay_ms(700);
 	set_diodes(diod_port, 0);
 }
-void calculus(port_name_t diod_port, uint8_t* status, int* memory){
+void calculus_option_set_lower(uint8_t* option, uint8_t value){
+	*option &= 0xF0;
+	*option |= (value & 0x0F);
+}
+uint8_t calculus_option_get_lower(uint8_t* option){
+	return *option & 0x0F;
+}
+void calculus_option_set_upper(uint8_t* option, uint8_t value){
+	*option &= 0x0F;
+	*option |= (value & 0xF0);
+}
+uint8_t calculus_option_get_upper(uint8_t* option){
+	return *option & 0xF0;
+}
+void calculus_input_num(uint8_t* buffer, uint8_t* option, char key){
+	calculus_feedback(PORT_B, *buffer * 10);
+	*buffer = key + (*buffer)*10;
+}
+void calculus_operate(port_name_t diod_port, uint8_t* buffer, uint8_t* option, uint8_t* memory){
+	switch(calculus_option_get_upper(option)){
+		case 0:	// start
+			*memory += *buffer;
+			*buffer = 0;
+			calculus_feedback(diod_port, CLC_FDB_DEF);
+		break;
+		case CLC_OPT_ADD:	// +
+			*memory += *buffer;
+			*buffer = 0;
+			*option = 0;
+		break;
+		case CLC_OPT_SUB:	// -
+			*memory -= *buffer;
+			*buffer = 0;
+			*option = 0;
+		break;
+		case CLC_OPT_MUL:	// *
+			*memory *= *buffer;
+			*buffer = 0;
+			*option = 0;
+		break;
+		case CLC_OPT_DIV:	// /
+			*memory /= *buffer;
+			*buffer = 0;
+			*option = 0;
+		break;
+		default:	//exception
+			calculus_feedback(diod_port, CLC_FDB_EXC);
+			*buffer = 0;
+			*option = 0;
+		break;
+	}
+}
+void calculus(port_name_t diod_port, uint8_t* buffer, uint8_t* option, uint8_t* memory){
 	char key = get_key();
 
 	if(key != 0){
-		switch(*status){
-			case 0:	//start
-				*memory = key;
-				*status = 1;
-				calculus_feedback(diod_port, 0xFF);
+		switch(key){
+			case 10:	//0 input
+				calculus_input_num(buffer, option, 0);
 			break;
-			case 1:	//operation expected
-				*status = key;
-				calculus_feedback(diod_port, 0xFF);
-				break;
-			case 13:	//+ operation
-				*memory += key;
-				*status = 1;
-				calculus_feedback(diod_port,1);
-			break;
-			case 14:	//- operation
-				*memory -= key;
-				*status = 1;
-				calculus_feedback(diod_port,2);
-			break;
-			case 15:	//`* operation
-				*memory *= key;
-				*status = 1;
-				calculus_feedback(diod_port,4);
+			case 11:	//reset
+				*buffer = 0;
+				*option = 0;
+				*memory = 0;
+				calculus_feedback(diod_port, CLC_FDB_RST);
 			break;
 			case 12:	//`/ operation
-				*memory /= key;
-				*status = 1;
-				calculus_feedback(diod_port,6);
+				calculus_operate(diod_port, buffer, option, memory);
+				calculus_option_set_upper(option,CLC_OPT_DIV);
 			break;
-			default:	//= operation
-				*status = 0;
-				set_diodes(diod_port, *memory);
+			case 13:	//+ operation
+				calculus_operate(diod_port, buffer, option, memory);
+				calculus_option_set_upper(option,CLC_OPT_ADD);
+			break;
+			case 14:	//- operation
+				calculus_operate(diod_port, buffer, option, memory);
+				calculus_option_set_upper(option,CLC_OPT_SUB);
+			break;
+			case 15:	//`* operation
+				calculus_operate(diod_port, buffer, option, memory);
+				calculus_option_set_upper(option,CLC_OPT_MUL);
+			break;
+			case 16:	//= operation
+				calculus_feedback(diod_port, CLC_FDB_RES);
+				calculus_operate(diod_port, buffer, option, memory);
+			break;
+			default:
+				calculus_input_num(buffer, option, key);
 			break;
 		}
 	} else {
 		set_diodes(diod_port, *memory);
+		set_diodes(PORT_B, *option);		//debug
 	}
 }
 
@@ -121,10 +184,9 @@ int main(void)
 
 	DDRA = 0xF0;	//wierszy(PA0-3) na wejście, kolumny(PA4-7) na wyjście
 	PORTA = 0x0F;
-	uint8_t calc_sts = 0;
-	int calc_mem = 0;
+	uint8_t calc_buf = 0, calc_opt = 0, calc_mem = 0;
 
 	while(1){
-		calculus(diod_port, &calc_sts, &calc_mem);
+		calculus(diod_port, &calc_buf, &calc_opt, &calc_mem);
 	}
 }
