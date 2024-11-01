b44cb7996389eb085c087fb9a376936adba19af9
Volodymyr <134626794+volodymyr-tsukanov@users.noreply.github.com>
2024-11-01 14:25:12 +0100

    added license cw3

--- lab/cw3/main.c
+++ lab/cw3/main.c
@@ -1,9 +1,20 @@
 /*
- * CW3
- *
- * Created: 17.10.2024 08:27:59
- * Author : volodymyr-tsukanov
- */
+    simple keypad calculator that uses an AVR ATMega32 microcontroller.
+    Copyright (C)  2024  volodymyr-tsukanov
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 #define F_CPU 4000000	//częstotliwość procesora dla delay
 
 #define CLC_OPT_ADD 0b00010000	//16
